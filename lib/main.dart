61100f8bb92e0b631d62976cffd359de6eb58f6d
Christian Granado Rodriguez <christian.g@smartwaystudio.com>
2023-05-01 15:42:08 +0100

    Added button knowledge, still don't know what im doing but...

--- lib/main.dart
+++ lib/main.dart
@@ -26,31 +26,57 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // Un container solo puede alojar un hijo, mientras que el column puede alojar varios
+      // con ctrl . puedes ver como envolverlo en un c
+      //  body: Container(
+      //   //Padding en el container
+      //   padding: const EdgeInsets.all(10),
+      //   child: Image.network(
+      //       'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'),
+      // ),
+      //Row or column reciben un array de widgets
+      // Las columnas y filas reciben todo el alto de la  pantalla por defecto
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: () {
+            var t = DateTime.now();
+            print(t);
+          },
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const <Widget>[
+                Icon(Icons.access_time_filled),
+                Text("Now"),
+              ]),
         ),
-        // Un container solo puede alojar un hijo, mientras que el column puede alojar varios
-        // con ctrl . puedes ver como envolverlo en un c
-        //  body: Container(
-        //   //Padding en el container
-        //   padding: const EdgeInsets.all(10),
-        //   child: Image.network(
-        //       'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'),
-        // ),
-        //Row or column reciben un array de widgets
-        // Las columnas y filas reciben todo el alto de la  pantalla por defecto
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.max,
-          children: const <Widget>[
-            SizedBox(
-              child: Text(
-                'Hola 1',
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Text('Hola 2'),
-          ],
-        ));
+      ),
+    );
+
+    // body: Row(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   mainAxisSize: MainAxisSize.max,
+    //   children: <Widget>[
+    //     SizedBox(
+    //       height: 50,
+    //       width: MediaQuery.of(context).size.width * 0.5,
+    //       child: const Text(
+    //         'Hola peter',
+    //         textAlign: TextAlign.center,
+    //       ),
+    //     ),
+    //     const Text('Hola puto1'),
+    //     ElevatedButton(
+    //       child: Text('A'),
+    //       onPressed: null,
+    //     )
+    //   ],
+    // ));
   }
 }
