109bb25f715ddfe0b743e718f6374978eb47d457
Christian Granado Rodriguez <christian.g@smartwaystudio.com>
2023-05-01 14:43:52 +0100

    Added row knowledge

--- lib/main.dart
+++ lib/main.dart
@@ -26,27 +26,31 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      // Un container solo puede alojar un hijo, mientras que el column puede alojar varios
-      // con ctrl . puedes ver como envolverlo en un column
-      //  body: Container(
-      //   //Padding en el container
-      //   padding: const EdgeInsets.all(10),
-      //   child: Image.network(
-      //       'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'),
-      // ),
-      body: ListView(
-        children: [
-          Container(
-            //Padding en el container
-            padding: const EdgeInsets.all(10),
-            child: Image.network(
-                'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        // Un container solo puede alojar un hijo, mientras que el column puede alojar varios
+        // con ctrl . puedes ver como envolverlo en un c
+        //  body: Container(
+        //   //Padding en el container
+        //   padding: const EdgeInsets.all(10),
+        //   child: Image.network(
+        //       'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'),
+        // ),
+        //Row or column reciben un array de widgets
+        // Las columnas y filas reciben todo el alto de la  pantalla por defecto
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          children: const <Widget>[
+            SizedBox(
+              child: Text(
+                'Hola 1',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text('Hola 2'),
+          ],
+        ));
   }
 }
